3d7f78676487013ec839d4bfef6ea13bfe612ce3
Mahesh Kumar Gutam <mahesh@Maheshs-MacBook-Air.local>
2025-07-07 18:41:44 +0530

    Fixing issue with theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,18 @@
+import './globals.css';
 import { ThemeProviderWrapper } from '../components/theme-provider';
-import './global.css'; // adjust path if needed
+
 export const metadata = {
   title: 'Mahesh Kumar Gutam Portfolio',
-  description: 'Showcasing Mahesh Kumar Gutam\'s experience, skills, and projects.',
+  description: "Showcasing Mahesh Kumar Gutam's experience, skills, and projects.",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProviderWrapper>
           {children}
-        </ThemeProvider>
+        </ThemeProviderWrapper>
       </body>
     </html>
   );
