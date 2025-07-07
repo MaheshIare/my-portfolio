46cdf77e3f01ba602481f3c4fee82e2e68bea38e
Mahesh Kumar Gutam <mahesh@Maheshs-MacBook-Air.local>
2025-07-07 18:09:48 +0530

    Add tailwind support

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import './global.css'; // adjust path if needed
 export const metadata = {
   title: 'Mahesh Kumar Gutam Portfolio',
   description: 'Showcasing Mahesh Kumar Gutam\'s experience, skills, and projects.',
