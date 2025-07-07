9484e875dd46e4f61b1bd031273e587f8e739fd7
Mahesh Kumar Gutam <39757128+MaheshIare@users.noreply.github.com>
2025-07-07 17:07:04 +0530

    Create layout.tsx
    Signed-off-by: Mahesh Kumar Gutam <39757128+MaheshIare@users.noreply.github.com>

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: 'Mahesh Kumar Gutam Portfolio',
+  description: 'Showcasing Mahesh Kumar Gutam\'s experience, skills, and projects.',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
